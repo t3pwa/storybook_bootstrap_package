a24891e1201da4a416006d26da19c64aef2ac942
Alexander Reinhard <alex@fabbing.com>
2024-04-10 07:02:20 +0200

    [TASK] Updates stories

--- stories/PageVariables.js
+++ stories/PageVariables.js
@@ -1,12 +1,19 @@
-import './page.css';
+//import './page.scss';
 import { createHeader } from './Header';
-import {Debug, Primary, Tertiary} from "./Fluid/ContentElements/panel.stories";
+
+// import { createPanel } from './Fluid/ContentElements/Panel';
+
+import {Debug, PrimaryPanel, Tertiary} from "./Fluid/ContentElements/panel.stories";
+
 import {VariablesDefault} from "./Fluid/Variables.stories";
+
+export const style = require('sass-extract-loader!./_variables.scss');
 
 export const createVariablesPage = () => {
   const article = document.createElement('article');
   let user = null;
   let header = null;
+  let panel = null;
 
   const rerenderHeader = () => {
     const wrapper = document.getElementsByTagName('article')[0];
@@ -32,8 +39,17 @@ export const createVariablesPage = () => {
     return createHeader({ onLogin, onLogout, onCreateAccount, user });
   };
 
+  const createPanelElement = () => {
+
+    return PrimaryPanel();
+  };
+
   header = createHeaderElement();
   article.appendChild(header);
+
+  panel = createPanelElement();
+
+  // article.appendChild(panel);
 
   const sectionMain = `
     <section class="main">
@@ -60,17 +76,59 @@ export const createVariablesPage = () => {
   const VariablesDefaultJsonSettings = JSON.parse(VariablesDefaultJsonSettingsString);
 
 
+  const style = require('sass-extract-loader!./_variables.scss');
+
+
+
+//  const brandInfo = style.global['$theme-colors'].value.primary.value.hex; // '#00695F';
+//  const brandInfo = style.global['$theme-colors'].value.primary.value.hex;
+
+  console.log(style.global);
+
+  console.log(JSON.stringify(style.global));
+//  console.log(typeof ( style.global) ) ;
+
+  let SassExtractedVariablesJson = {};
+  let assExtractedVariablesString = '';
+  let SassExtractedVariablesHtmlOutput = `<div class="row">`;
+
+  for (const key in style.global ) {
+    console.log(`${key}: ${style.global[key]}`);
+
+    // console.log( `${style.global[key].type}` );
+
+    // @ToDo: Demand Filter: If value in array [ white, gray-100, gray-200, gray-300, gray-400, gray-500, gray-600, gray-700, gray-800, gray-900, black
+    // primary, secondary, tertiary, quaternary, default, success, info, warning, danger
+
+
+    if ( style.global[key].type == 'SassNumber' ) {
+      console.log( ' ', key, style.global[key].value + style.global[key].unit );
+    } else if ( style.global[key].type == 'SassColor' ) {
+      console.log( ' ', key, style.global[key].value.hex );
+      SassExtractedVariablesHtmlOutput += `<div className="p-3 mb-3 text-white" style="background: `+ style.global[key].value.hex +`  ;">`+key+`</div>`
+    }
+
+  };
+  SassExtractedVariablesHtmlOutput += `</div>`;
+
+
 
   const sectionDebug = `
     <section class="debug">
+    
+    
+        
+    
       <h4>Variables Debug</h4>
-      <pre>https://getbootstrap.com/docs/5.0/customize/color/</pre>
-      
-      <h3>Theme colors</h3>
-      <pre>
-  `
-  + `${VariablesDefaultJsonSettingsString}`
-  + `</pre><hr></section>`;
+      <a href="https://getbootstrap.com/docs/5.0/customize/color/">https://getbootstrap.com/docs/5.0/customize/color/</a>
+      <h3>Style Global Colors</h3>
+      <pre>`
+//  + `${VariablesDefaultJsonSettingsString}`
+      + `${JSON.stringify(style.global, null, "\t")}`
+    + `</pre>
+      <hr>`
+    + ``
+  + `</section>`;
 
   article.insertAdjacentHTML('beforeend', sectionDebug);
 
@@ -79,8 +137,6 @@ export const createVariablesPage = () => {
   Object.keys(VariablesDefaultJsonSettings.scss).forEach(function(k) {
       // console.log(k + ' - ' + VariablesDefaultJsonSettings.scss[k]);
 
-    // @ToDo: Demand Filter: If value in array [ white, gray-100, gray-200, gray-300, gray-400, gray-500, gray-600, gray-700, gray-800, gray-900, black
-    // primary, secondary, tertiary, quaternary, default, success, info, warning, danger
 
 
     // @ToDo: if VariablesDefaultJsonSettings.scss[k] is (html) color, starts with # 6
@@ -91,7 +147,8 @@ export const createVariablesPage = () => {
     // article.insertAdjacentHTML('beforeend',"</div>
 
 
-  article.insertAdjacentHTML('beforeend', VariablesDefaultHtmlOutput + `</div>`);
+  // article.insertAdjacentHTML('beforeend', VariablesDefaultHtmlOutput + `</div>`);
+  article.insertAdjacentHTML('beforeend', SassExtractedVariablesHtmlOutput );
 
 //  article.insertAdjacentHTML('beforeend', sectionMain);
 
